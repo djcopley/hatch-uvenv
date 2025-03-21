fe7e18eadaa160dfe766448a41cddf73804bc72c
Daniel Copley <djcopley@proton.me>
2025-03-20 20:17:25 -0400

    use self.uv_path for uv sync
    closes #1

--- src/hatch_uvenv/plugin.py
+++ src/hatch_uvenv/plugin.py
@@ -219,7 +219,7 @@ class UVVirtualEnvironmentPlugin(EnvironmentInterface):
             yield
 
     def construct_uv_sync_command(self):
-        command = ["uv", "sync", "--active", "--locked"]
+        command = [self.uv_path, "sync", "--active", "--locked"]
 
         # Default to -1 verbosity
         add_verbosity_flag(command, self.verbosity, adjustment=-1)
